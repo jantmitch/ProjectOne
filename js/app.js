264f4d4eb614bd88a266a5b4cf67f82ddd693748
Max Liao <max.liao@gmail.com>
2018-03-27 20:43:01 -0400

    Map tutorial 2

--- js/app.js
+++ js/app.js
@@ -12,18 +12,49 @@ firebase.initializeApp(config);
 // Create a variable to reference the database
 var database = firebase.database();
 
-//Google Map
+//Google Maps
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
+      zoom: 3,
+      center: {lat: -28.024, lng: 140.887}
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+
+    // Create an array of alphabetical characters used to label the markers.
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    var markers = locations.map(function(location, i) {
+        console.log(location);
+        console.log(i);
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length]
+      });
     });
+
+    // var marker = new google.maps.Marker({
+    //   position: uluru,
+    //   map: map
+    // });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
   }
+
+    
+  var uluru = {lat: -25.363, lng: 131.044};
+  var locations = [
+    {lat: -31.563910, lng: 147.154312},
+    {lat: -33.718234, lng: 150.363181},
+    {lat: -33.727111, lng: 150.371124},
+    {lat: -33.848588, lng: 151.209834},
+    {lat: -43.999792, lng: 170.463352},
+    uluru
+  ]
 
 // Clear Firebase
 $("#clearfirebase").on("click", function(event) {
