53fcca0537df0f6c185351e90112bb9c48521f77
Max Liao <max.liao@gmail.com>
2018-03-27 20:18:18 -0400

    Map tutorial 1

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBIevTR-mQFdjL0Up4vopFmlROoboi0p9I",
-    authDomain: "test-4e111.firebaseapp.com",
-    databaseURL: "https://test-4e111.firebaseio.com",
-    projectId: "test-4e111",
-    storageBucket: "test-4e111.appspot.com",
-    messagingSenderId: "50749461627"
-  };
-  firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyCAQ69TwLNNWW4TRn0zYsQV2GL2Tio4kSA",
+    authDomain: "first-pro-a1a2e.firebaseapp.com",
+    databaseURL: "https://first-pro-a1a2e.firebaseio.com",
+    projectId: "first-pro-a1a2e",
+    storageBucket: "first-pro-a1a2e.appspot.com",
+    messagingSenderId: "486106310535"
+};
+firebase.initializeApp(config);
 
 // Create a variable to reference the database
 var database = firebase.database();
 
+//Google Map
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
 
 // Clear Firebase
 $("#clearfirebase").on("click", function(event) {
     database.ref().set({});
 });
-
 
 // At the initial load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
@@ -46,6 +57,7 @@ var createRow = function(){
   };
 
 // This function handles events where the submit button is clicked
+
 $("#submit").on("click", function(event) {
     // event.preventDefault() prevents submit button from trying to send a form.
     // Using a submit button instead of a regular button allows the user to hit
