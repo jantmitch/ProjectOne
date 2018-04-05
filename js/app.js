4a8b1a3591d7be0712b8b252de95c3641b11d526
Max Liao <max.liao@gmail.com>
2018-04-05 18:51:22 -0400

    working map

--- js/app.js
+++ js/app.js
@@ -34,6 +34,8 @@ var locations = [
 ];
 
 var locationsobj = {};
+var namesarray = [];
+var namesobj = {};
 
 //On click functions
 // Clear Firebase
@@ -41,12 +43,16 @@ $("#clearfirebase").on("click", function(event) {
     database.ref().remove();
     $("#Table").empty();
     locations = [];
+    namesarray = [];
 });
 
-var k = 0;
+// var k = 0;
+
+// This function handles events where the submit button is clicked
+
+
 // This function handles events where the submit button is clicked
 $("#submit").on("click", function(event) {
-    
     // event.preventDefault() prevents submit button from trying to send a form.
     // Using a submit button instead of a regular button allows the user to hit
     // "Enter" instead of clicking the button if desired
@@ -54,38 +60,92 @@ $("#submit").on("click", function(event) {
     
     var location = $("#location-input").val().trim();
     var geocodeQuery = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + googlemapskey;
-    
-    $.ajax({
-      url: geocodeQuery,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        var latit = response.results[0].geometry.location.lat;
-        var longi = response.results[0].geometry.location.lng;
-        var add = response.results[0].formatted_address;
-        var coordinates = {lat: latit, lng: longi};
 
-        if (location !== ""){
-            // Save the new post in Firebase
-            database.ref(i).set({
-                name: location,
-                address: add,
-                location: coordinates,
-                index: i
-            });
-            i++;
+    var repeat = false;
+    if (location == ""){
+        console.log("no Input");
+        repeat = true;
+    }
+    for (i=0; i<namesarray.length; i++){
+        if (namesarray[i] == location){
+            console.log("name repeat");
+            repeat = true;
+            break;
+        // } else if (coordinates == locations[i]){
+        //     console.log("coordinates repeat");
+        //     break;
+        } else{
+            // console.log("not a repeat");
         }
-        initMap();
-    });
+    }
+    
+    if (repeat == false) {  
+        $.ajax({
+        url: geocodeQuery,
+        method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var latit = response.results[0].geometry.location.lat;
+            var longi = response.results[0].geometry.location.lng;
+            var add = response.results[0].formatted_address;
+            var coordinates = {lat: latit, lng: longi};
+
+            if (location !== ""){
+                // Save the new post in Firebase
+                database.ref(i).set({
+                    name: location,
+                    address: add,
+                    location: coordinates,
+                    index: i
+                });
+                i++;
+            }
+            initMap();
+        });
+    }
   
 });  
+// $("#submit").on("click", function(event) {
+    
+//     // event.preventDefault() prevents submit button from trying to send a form.
+//     // Using a submit button instead of a regular button allows the user to hit
+//     // "Enter" instead of clicking the button if desired
+//     event.preventDefault();
+    
+//     var location = $("#location-input").val().trim();
+//     var geocodeQuery = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + googlemapskey;
+    
+//     $.ajax({
+//       url: geocodeQuery,
+//       method: "GET"
+//     }).then(function(response) {
+//         // console.log(response);
+//         var latit = response.results[0].geometry.location.lat;
+//         var longi = response.results[0].geometry.location.lng;
+//         var add = response.results[0].formatted_address;
+//         var coordinates = {lat: latit, lng: longi};
+
+//         if (location !== ""){
+//             // Save the new post in Firebase
+//             database.ref(i).set({
+//                 name: location,
+//                 address: add,
+//                 location: coordinates,
+//                 index: i
+//             });
+//             i++;
+//         }
+//         initMap();
+//     });
+  
+// });  
 // Save input data to Firebase and save to table
 
 // At the initial load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
 database.ref().on("value", function(snapshot) {
-    console.log(snapshot.val().length);
-    console.log(snapshot.val());
+    // console.log(snapshot.val().length);
+    // console.log(snapshot.val());
     // console.log(locations);
     i = snapshot.val().length;  
     initMap();
@@ -101,31 +161,41 @@ database.ref().on("child_added", function(snapshot) {
     var newlocation = snapshot.val().location;
     // console.log(newlocation);
     locationsobj[j] = newlocation;
-    j++;
+    namesobj[j] = newname;
+
     locations = Object.keys(locationsobj).map(function(key) {
         // return [Number(key), locationsobj[key]];
         return locationsobj[key];
       });
-    createRow(newname,newaddress);
+
+    namesarray = Object.keys(namesobj).map(function(key) {
+        // return [Number(key), locationsobj[key]];
+        return namesobj[key];
+    });
+
+    createRow(newname,newaddress,j);
+    j++;
 });
 
 //Create Table from Firebase
-var createRow = function(name, address){
+var createRow = function(name, address, index){
     // Get reference to existing tbody element, create a new table row element
     var tBody = $("tbody");
-    var tRow = $("<tr>");
+    var tRow = $("<tr>").attr("id", index);
 
     // create and save a reference to a td in the same statement we update its text
     var name = $("<td>").text(name);
     var address = $("<td>").text(address);
+    var remove = $("<td>").html("<button class='remove' index="+ index + ">X</button>");
 
     // Append the newly created table data to the table row
-    tRow.append(name,address);
+    tRow.append(name,address,remove);
 
     // Append the table row to the table body
     tBody.append(tRow);
 
-    // Grab city/state
+    // Grab city/state. Split string by comma
+    console.log(address[0]);
     var addressstr = JSON.stringify(address[0]);
     console.log(addressstr);
     for (ind = 0; ind<addressstr.length;ind++){
@@ -133,9 +203,7 @@ var createRow = function(name, address){
             console.log("comma");
         }
     }
-
   };
-
 
 //Display Maps
 function initMap() {
@@ -147,6 +215,9 @@ function initMap() {
 
     // Create an array of alphabetical characters used to label the markers.
     var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+    // Marker variable
+    // var marker2 = new google.maps.Marker({position:atlanta});
 
     // Add some markers to the map.
     // Note: The code uses the JavaScript Array.prototype.map() method to
@@ -161,17 +232,6 @@ function initMap() {
 
     // Map events
 
-    google.maps.event.addListener(map, 'click', function(event) {
-        console.log("map click");
-    //     // placeMarker(map, event.latLng);
-        });
-
-
-    // var infowindwo
-    var infowindow = new google.maps.InfoWindow({
-        content:"Hello World!"
-    });
-    
     // var data;
     // map.addListener('click', function(e) {
     //     // console.log("map click");
@@ -179,23 +239,32 @@ function initMap() {
     //     // data.lng = e.latLng.lng();
     //     // addToFirebase(data);
     //   });
-    // console.log(marker[0].getPosition());
 
+    google.maps.event.addListener(map, 'click', function(event) {
+        console.log("map click");
+    //     // placeMarker(map, event.latLng);
+        });
+
+    var infowindow = new google.maps.InfoWindow({
+        content:"Hello World!"
+    });
+    
+    // console.log(marker[0].getPosition());
     var breakvar = false;
-    for (ind=0 ; ind< marker.length; ind++){
+    for (i=0 ; i< marker.length; i++){
         if (breakvar === true ){ 
             console.log("break");
-            console.log(ind);
+            console.log(i);
             breakvar = false;
             break;
         }
         google.maps.event.addListener(marker[i],'click',function() {
             console.log("marker click");
-            console.log(ind);
+            console.log(i);
             // console.log(locations[i - 1]);
 
             breakvar = true;
-            // infowindow.open(map, marker[4]);
+            infowindow.open(map, marker[4]);
             // console.log(marker);
             // map.setZoom(9);
             // console.log(marker[i].getPosition());
@@ -221,6 +290,93 @@ function initMap() {
     });
     infowindow.open(map,marker);
   } 
+
+
+
+// //Display Maps
+// function initMap() {
+//     console.log(locations);
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       zoom: 3,
+//       center: atlanta
+//     });
+
+//     // Create an array of alphabetical characters used to label the markers.
+//     var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+//     // Add some markers to the map.
+//     // Note: The code uses the JavaScript Array.prototype.map() method to
+//     // create an array of markers based on a given "locations" array.
+//     // The map() method here has nothing to do with the Google Maps API.
+//     var marker = locations.map(function(location, i) {
+//       return new google.maps.Marker({
+//         position: location,
+//         label: labels[i % labels.length]
+//       });
+//     });
+
+//     // Map events
+
+//     google.maps.event.addListener(map, 'click', function(event) {
+//         console.log("map click");
+//     //     // placeMarker(map, event.latLng);
+//         });
+
+
+//     // var infowindwo
+//     var infowindow = new google.maps.InfoWindow({
+//         content:"Hello World!"
+//     });
+    
+//     // var data;
+//     // map.addListener('click', function(e) {
+//     //     // console.log("map click");
+//     //     // data.lat = e.latLng.lat();
+//     //     // data.lng = e.latLng.lng();
+//     //     // addToFirebase(data);
+//     //   });
+//     // console.log(marker[0].getPosition());
+
+//     var breakvar = false;
+//     for (ind=0 ; ind< marker.length; ind++){
+//         if (breakvar === true ){ 
+//             console.log("break");
+//             console.log(ind);
+//             breakvar = false;
+//             break;
+//         }
+//         google.maps.event.addListener(marker[i],'click',function() {
+//             console.log("marker click");
+//             console.log(ind);
+//             // console.log(locations[i - 1]);
+
+//             breakvar = true;
+//             // infowindow.open(map, marker[4]);
+//             // console.log(marker);
+//             // map.setZoom(9);
+//             // console.log(marker[i].getPosition());
+//             // map.setCenter(locations[0]);
+//         }); 
+//     }
+    
+
+//     // Add a marker clusterer to manage the markers.
+//     var markerCluster = new MarkerClusterer(map, marker,
+//         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+//   }
+
+
+//   function placeMarker(map, location) {
+//     // var marker = new google.maps.Marker({
+//     //   position: location,
+//     //   map: map
+//     // });
+//     var infowindow = new google.maps.InfoWindow({
+//       content: 'Latitude: ' + location.lat() +
+//       '<br>Longitude: ' + location.lng()
+//     });
+//     infowindow.open(map,marker);
+//   } 
 
   
 
